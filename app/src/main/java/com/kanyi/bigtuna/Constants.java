528bf7f7aa70bfee9213b7c9dcb58bcf8415cf90
Sidney Kanyi <sidney.kanyi@strathmore.edu>
2022-01-27 15:46:32 +0300

    Seller Add Product

---
+++ app/src/main/java/com/kanyi/bigtuna/Constants.java
@@ -0,0 +1,15 @@
+package com.kanyi.bigtuna;
+
+public class Constants {
+
+    //product categories
+    public static final String[] productCategories = {
+            "Domestic(3000L-5000L)",
+            "Functions/Events",
+            "Dispensed Water",
+            "Other related products"
+
+    };
+
+
+}
